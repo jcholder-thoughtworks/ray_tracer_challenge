b3d9d414ba60ae5c5c1514a690df85840b22773e
JC Holder <jc.holder@thoughtworks.com>
2020-01-28 17:16:31 -0500

    Example use of Point, Vector

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,41 @@
 use ray_tracer_challenge::*;
 
-fn main() {
-    let point = Point { x: 1.0, y: 1.0, z: 1.0 };
-    let vector = Vector { x: 1.0, y: 1.0, z: 1.0 };
+#[derive(Clone,Debug)]
+struct Projectile {
+    position: Point,
+    velocity: Vector,
+}
 
-    println!("It's a point! {:?}", point);
-    println!("It's a vector! {:?}", vector);
-    println!("Let's add them together! {:?}", point + vector);
+#[derive(Clone,Debug)]
+struct Environment {
+    gravity: Vector,
+    wind: Vector,
+}
+
+fn tick(environment: &Environment, projectile: Projectile) -> Projectile {
+    let position = projectile.position + projectile.velocity.clone();
+    let velocity = projectile.velocity + environment.gravity + environment.wind;
+
+    Projectile { position, velocity }
+}
+
+fn main() {
+    let mut projectile = Projectile {
+        position: Point { x: 1.0, y: 1.0, z: 1.0 },
+        velocity: Vector { x: 1.0, y: 1.0, z: 1.0 },
+    };
+    let environment = Environment {
+        gravity: Vector { x: 0.0, y: -0.1, z: 0.0 },
+        wind: Vector { x: 0.1, y: 0.0, z: 0.0 },
+    };
+
+    println!("It's a projectile! {:?}", projectile);
+    println!("It's an environment! {:?}", environment);
+
+    while projectile.position.y > 0.0 {
+        projectile = tick(&environment, projectile);
+        println!("Updated projectile! {:?}", projectile);
+    }
+
+    println!("And we've landed!")
 }
