b74009c6b52068b77b7a7502f81baa7aba916408
JC Holder <jc.holder@thoughtworks.com>
2020-01-28 17:02:48 -0500

    Add example module usage to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use ray_tracer_challenge::*;
+
 fn main() {
-    println!("Hello, world!");
+    let point = Point { x: 1.0, y: 1.0, z: 1.0 };
+    let vector = Vector { x: 1.0, y: 1.0, z: 1.0 };
+
+    println!("It's a point! {:?}", point);
+    println!("It's a vector! {:?}", vector);
+    println!("Let's add them together! {:?}", point + vector);
 }
