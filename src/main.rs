f5dc4c43bf9258c48b1fd206ab06afb504b8a81e
JC Holder <jc.holder@thoughtworks.com>
2020-01-28 14:57:09 -0500

    Prototype equalish and test it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+const EPSILON: f32 = 0.00001;
+
 struct Point {
     pub x: f32,
     pub y: f32,
@@ -10,6 +12,25 @@ struct Vector {
     pub z: f32,
 }
 
+fn equalish(a: f32, b: f32) -> bool {
+    (a - b).abs() < EPSILON
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn equalish_is_true_for_diff_below_epsilon() {
+        assert!(equalish(1.0, 1.000001));
+    }
+
+    #[test]
+    fn equalish_is_false_for_diff_above_epsilon() {
+        assert!(! equalish(1.0, 1.00002));
+    }
 }
