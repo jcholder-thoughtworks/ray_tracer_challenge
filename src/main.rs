3137d40a3e704430740bfeb2b9e843e86706f692
JC Holder <jc.holder@thoughtworks.com>
2020-01-28 15:04:44 -0500

    Add Point#equalish_to and tests

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,14 @@ struct Vector {
     pub z: f32,
 }
 
+impl Point {
+    pub fn equalish_to(&self, other: &Point) -> bool {
+        equalish(self.x, other.x) &&
+            equalish(self.y, other.y) &&
+            equalish(self.z, other.z)
+    }
+}
+
 fn equalish(a: f32, b: f32) -> bool {
     (a - b).abs() < EPSILON
 }
@@ -32,5 +40,43 @@ mod tests {
     #[test]
     fn equalish_is_false_for_diff_above_epsilon() {
         assert!(! equalish(1.0, 1.00002));
+    }
+
+    mod point_tests {
+        use super::*;
+
+        #[test]
+        fn equalish_is_true_for_diff_below_epsilon() {
+            let point_a = Point {
+                x: 1.0,
+                y: 1.0,
+                z: 1.0,
+            };
+
+            let point_b = Point {
+                x: 1.000001,
+                y: 1.000001,
+                z: 1.000001,
+            };
+
+            assert!(point_a.equalish_to(&point_b));
+        }
+
+        #[test]
+        fn equalish_is_false_for_diff_above_epsilon() {
+            let point_a = Point {
+                x: 1.0,
+                y: 1.0,
+                z: 1.0,
+            };
+
+            let point_b = Point {
+                x: 1.00002,
+                y: 1.00002,
+                z: 1.00002,
+            };
+
+            assert!(! point_a.equalish_to(&point_b));
+        }
     }
 }
