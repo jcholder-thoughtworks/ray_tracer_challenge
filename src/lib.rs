6e4bce1f70a8ac0b0dc20df77fef5887f8f1893e
JC Holder <jc.holder@thoughtworks.com>
2020-02-10 12:00:59 -0500

    Set defaults for Material expected by features

--- src/lib.rs
+++ src/lib.rs
@@ -425,11 +425,11 @@ pub struct Material {
 impl Material {
     pub fn new() -> Self {
         Self {
-            color: Color::new(0.0, 0.0, 0.0),
-            ambient: 0.0,
-            diffuse: 0.0,
-            specular: 0.0,
-            shininess: 0.0,
+            color: Color::new(1.0, 1.0, 1.0),
+            ambient: 0.1,
+            diffuse: 0.9,
+            specular: 0.9,
+            shininess: 200.0,
         }
     }
 }
